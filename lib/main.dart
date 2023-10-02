e5a2ac8b6652354d7d2148afed2e444d8e404110
Vishnu kancharla <vishnu.kancharla11@gmail.com>
2023-10-02 12:26:52 +0530

    changed meapp to myapp

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:musicplayer/music_player_view.dart';
 import 'package:musicplayer/songs_list_view.dart';
 
-import 'meapp.dart';
+import 'my_app.dart';
 
 void main() {
   runApp(const MyApp());
